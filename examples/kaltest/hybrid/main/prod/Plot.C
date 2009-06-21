70f71627281b3502893b514a72317187c1ff60a6
Keisuke Fujii <keisuke.fujii@kek.jp>
2009-06-21 04:16:12 +0000

    2009/06/21 K.Fujii

--- examples/kaltest/hybrid/main/prod/Plot.C
+++ examples/kaltest/hybrid/main/prod/Plot.C
@@ -14,9 +14,6 @@ void Plot(int layer=259, const char *fname="p-100.0t000.10k.root")
        << ">>h1" << ends;
   track->Draw(str1.str().data(),"");
   h1->FitSlicesY();
-  h1_2->SetMarkerStyle(4);
-  h1_2->SetMarkerSize(1);
-  h1_2->Draw();
 
   TH2D *h2 = new TH2D("h2","",15,0.,250.,10,-0.05,+0.05);
   stringstream str2;
@@ -25,10 +22,14 @@ void Plot(int layer=259, const char *fname="p-100.0t000.10k.root")
        << ">>h2" << ends;
   track->Draw(str2.str().data(),"");
   h2->FitSlicesY();
-  h2_2->Draw();
-  h2_2->SetMinimum(0.);
-  h2_2->SetMaximum(0.03);
+
+  h1_2->SetMinimum(0.);
+  h1_2->SetMaximum(0.03);
+  h1_2->SetMarkerStyle(4);
+  h1_2->SetMarkerSize(1);
+  h1_2->Draw();
+
   h2_2->SetMarkerStyle(5);
   h2_2->SetMarkerSize(1);
-  h1_2->Draw("same");
+  h2_2->Draw("same");
 }
